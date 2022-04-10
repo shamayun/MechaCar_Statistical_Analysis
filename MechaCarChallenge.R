bff97854ae90a0233f53a8a1ec8f93abd0af65d8
Shamay Bahar <96354508+shamayun@users.noreply.github.com>
2022-04-10 18:44:07 -0400

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,30 +7,27 @@ library(dplyr)
 MechaCar_mpg <- read.csv(file="MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 
 #  linear regression model on all six variables [15.7.2(single)/15.7.3(multiple)]
-lm(MechaCar$mpg ~ MechaCar$vehicle_length + 
-              MechaCar$vehicle_weight + 
-              MechaCar$spoiler_angle +
-              MechaCar$ground_clearance +
-              MechaCar$AWD, MechaCar)
+lm(MechaCar_mpg$mpg ~ MechaCar_mpg$vehicle_length + 
+              MechaCar_mpg$vehicle_weight + 
+              MechaCar_mpg$spoiler_angle +
+              MechaCar_mpg$ground_clearance +
+              MechaCar_mpg$AWD, MechaCar_mpg)
 
 #Summary Function for Multiple Linear Regression[15.7.3]
-summary(lm(MechaCar$mpg ~ MechaCar$vehicle_length + 
-             MechaCar$vehicle_weight + 
-             MechaCar$spoiler_angle +
-             MechaCar$ground_clearance +
-             MechaCar$AWD, MechaCar))
+summary(lm(MechaCar_mpg$mpg ~ MechaCar_mpg$vehicle_length + 
+             MechaCar_mpg$vehicle_weight + 
+             MechaCar_mpg$spoiler_angle +
+             MechaCar_mpg$ground_clearance +
+             MechaCar_mpg$AWD, MechaCar_mpg))
 
 
 #Deliverable 2: Create Visualizations for the Trip Analysis
-#Collect summary statistics on the pounds per square inch (PSI) of the [SUSPENSION COILS] from the manufacturing lots
+#Collect summary statistics on the PSI of the [SUSPENSION COILS] from the manufacturing lots
 # Import and read suspension_coil dataset as a dataframe
 suspension_coil <- read.csv(file="Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
 
 #Getting mean, median, variance, and standard deviation of the suspension coil's PSI column-15.2.5
-total_summary <- suspension_coil %>% summarize(Mean = mean(PSI),
-                                               Median = median(PSI),
-                                               Variance = var(PSI),
-                                               SD = sd(PSI))
+total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 
 total_summary
 
@@ -42,6 +39,12 @@ lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mea
 
 #Deliverable 3: T-Tests on Suspension Coils
 
-# t-test-15.4.1, example
+# t-test[15.4.1 & 15.6.3 & 15.6.4]
 #to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 PSI
 t.test(suspension_coil$PSI, mu=1500)
+
+# t-test for all 3 lots
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+
